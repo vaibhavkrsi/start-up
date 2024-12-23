32cb2708f196c5e30f585af2f0852b459e7223ce
Vaibhav Kumar Singh <hyperv117@gmail.com>
2024-12-23 13:29:56 +0530

    second

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export", // Enables static export
+  trailingSlash: true, // Optional: Adds trailing slashes to URLs
   images: {
-    domains: ["localhost"],
+    domains: ["localhost"], // Add any local development image domains
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "cdn.sanity.io",
+        hostname: "cdn.sanity.io", // Allow images from Sanity's CDN
         port: "",
       },
     ],
